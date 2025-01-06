e3f3a4fdfd742bf7ba8b25980f303b012176b442
iliaschair <iliaschair14@gmail.com>
2025-01-06 17:24:58 +0100

    fix indentation

--- kinbot/tpl/ase_sella_ts_end.tpl.py
+++ kinbot/tpl/ase_sella_ts_end.tpl.py
@@ -15,38 +15,38 @@ from kinbot.frequencies import get_frequencies
 
 
 def calc_vibrations(mol):
-        mol.calc = Gaussian(
-            mem='8GB',
-            nprocshared=1,
-            method='b3lyp',
-            basis='tzvp',
-            chk="{label}.chk",
-            label="{label}",
-        )
-        mol.calc.label = '{label}_vib'
-        if 'chk' in mol.calc.parameters:
-            del mol.calc.parameters['chk']
-        # Compute frequencies in a separate temporary directory to avoid 
-        # conflicts accessing the cache in parallel calculations.
-        if not os.path.isdir('{label}_vib'):
-            os.mkdir('{label}_vib')
-        init_dir = os.getcwd()
-        os.chdir('{label}_vib')
-        if os.path.isdir('vib'):
-            shutil.rmtree('vib')
-        vib = Vibrations(mol)
-        vib.run()
-        # Use kinbot frequencies to avoid mixing low vib frequencies with 
-        # the values associated with external rotations.
-        _ = vib.get_frequencies()
-        zpe = vib.get_zero_point_energy() * EVtoHARTREE
-        hessian = vib.H / 97.17370087
-        st_pt = StationaryPoint.from_ase_atoms(mol)
-        st_pt.characterize()
-        freqs, _ = get_frequencies(st_pt, hessian, st_pt.geom)
-        os.chdir(init_dir)
-        shutil.rmtree('{label}_vib')
-        return freqs, zpe, hessian
+    mol.calc = Gaussian(
+        mem='8GB',
+        nprocshared=1,
+        method='b3lyp',
+        basis='tzvp',
+        chk="{label}.chk",
+        label="{label}",
+    )
+    mol.calc.label = '{label}_vib'
+    if 'chk' in mol.calc.parameters:
+        del mol.calc.parameters['chk']
+    # Compute frequencies in a separate temporary directory to avoid 
+    # conflicts accessing the cache in parallel calculations.
+    if not os.path.isdir('{label}_vib'):
+        os.mkdir('{label}_vib')
+    init_dir = os.getcwd()
+    os.chdir('{label}_vib')
+    if os.path.isdir('vib'):
+        shutil.rmtree('vib')
+    vib = Vibrations(mol)
+    vib.run()
+    # Use kinbot frequencies to avoid mixing low vib frequencies with 
+    # the values associated with external rotations.
+    _ = vib.get_frequencies()
+    zpe = vib.get_zero_point_energy() * EVtoHARTREE
+    hessian = vib.H / 97.17370087
+    st_pt = StationaryPoint.from_ase_atoms(mol)
+    st_pt.characterize()
+    freqs, _ = get_frequencies(st_pt, hessian, st_pt.geom)
+    os.chdir(init_dir)
+    shutil.rmtree('{label}_vib')
+    return freqs, zpe, hessian
 
 
 db = connect('{working_dir}/kinbot.db')
